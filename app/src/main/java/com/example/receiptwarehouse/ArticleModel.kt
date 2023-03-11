256d89ddd8b608253b2fda33c22fd44762316399
Mateusz Zwierzchowski <zwierzchowski.email@gmail.com>
2023-03-11 19:13:21 +0100

    Added database

---
+++ app/src/main/java/com/example/receiptwarehouse/ArticleModel.kt
@@ -0,0 +1,19 @@
+package com.example.receiptwarehouse
+
+import io.realm.RealmModel
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.RealmClass
+import io.realm.annotations.Required
+
+@RealmClass
+class ArticleModel : RealmModel{
+    @PrimaryKey
+    var id :String? = ""
+
+    @Required
+    var title: String = ""
+
+    @Required
+    var description: String? = ""
+
+}
