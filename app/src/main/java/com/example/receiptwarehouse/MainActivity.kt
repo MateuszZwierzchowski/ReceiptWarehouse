1e55f6b7fa37358686c93cf85a93778cc474d2a0
acf380 <acf38000@gmail.com>
2023-03-17 12:26:47 +0100

    keep up 2

--- app/src/main/java/com/example/receiptwarehouse/MainActivity.kt
+++ app/src/main/java/com/example/receiptwarehouse/MainActivity.kt
@@ -29,8 +29,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            findNavController(R.id.nav_host_fragment_content_main).navigateUp()
         }
     }
 
